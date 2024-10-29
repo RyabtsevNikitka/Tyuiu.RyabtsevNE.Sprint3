85905d301ac362cfc4861a5c6599e10e38605555
Tyuiu.RyabtsevNE@outlook.com <github_090506@mail.ru>
2024-10-29 21:41:47 +0500

    Task6

---
+++ Tyuiu.RyabtsevNE.Sprint3.Task6.V23.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.RyabtsevNE.Sprint3.Task6.V23.Lib
+{
+    public class DataService : ISprint3Task6V23
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int res = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                for (int d = 1; d < 11; d++)
+                {
+                    if (x % d == 0)
+                    {
+                        res = res + 1;
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.RyabtsevNE.Sprint3.Task6.V23.Test/DataServiceTest.cs
@@ -0,0 +1,13 @@
+using Tyuiu.RyabtsevNE.Sprint3.Task6.V23.Lib;
+
+namespace Tyuiu.RyabtsevNE.Sprint3.Task6.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
