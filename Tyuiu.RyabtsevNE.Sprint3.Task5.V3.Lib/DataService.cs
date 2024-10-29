6ad6a78d8b97827ee1ec20126a84c56ab96b37a9
Tyuiu.RyabtsevNE@outlook.com <github_090506@mail.ru>
2024-10-29 21:35:46 +0500

    Task5

---
+++ Tyuiu.RyabtsevNE.Sprint3.Task5.V3.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.RyabtsevNE.Sprint3.Task5.V3.Lib
+{
+    public class DataService : ISprint3Task5V3
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double sumSeriec = 0;
+            int i, j;
+            for (i = startValue1; i <= stopValue1; i++)
+            {
+                for (j = startValue2; j <= stopValue2; j++)
+                {
+                    sumSeriec = sumSeriec + (Math.Sin(j) + x) / x;
+                }
+            }
+            return Math.Round(sumSeriec, 3);
+        }
+    }
+}
---
+++ Tyuiu.RyabtsevNE.Sprint3.Task5.V3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.RyabtsevNE.Sprint3.Task5.V3.Lib;
+
+namespace Tyuiu.RyabtsevNE.Sprint3.Task5.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Рябцев Н. Е. | СМАРТб-24-1";
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                  *");
+            Console.WriteLine("* Вложенные циклы                                                            *");
+            Console.WriteLine("* Задание #5                                                                 *");
+            Console.WriteLine("* Вариант #3                                                                 *");
+            Console.WriteLine("* Выполнил: Рябцев Никита Евгеньевич | СМАРТб-24-1                           *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                   *");
+            Console.WriteLine("* Найти                                                                      *");
+            Console.WriteLine("* Сумма сумм рада                                                           * ");
+            Console.WriteLine("*                                                                            *");
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                           *");
+            Console.WriteLine("******************************************************************************");
+
+            int x = 5;
+            int startValue1 = 1;
+            int startValue2 = 3;
+            int stopValue1 = 1;
+            int stopValue2 = 11;
+
+
+
+            Console.WriteLine("Переменная Х = " + x);
+            Console.WriteLine("Старт шага первой суммы ряда = " + startValue1);
+            Console.WriteLine("Конец шага первой суммы рада  = " + stopValue1);
+            Console.WriteLine("Старт шага второй суммы рада = " + startValue2);
+            Console.WriteLine("Конец шага второго суммы рада = " + stopValue2);
+
+
+            Console.WriteLine("******************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                 *");
+            Console.WriteLine("******************************************************************************");
+
+            Console.WriteLine("Сумма сумм рада = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+
+            Console.ReadKey();
+        }
+    }
+}
